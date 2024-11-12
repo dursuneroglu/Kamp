d50ed1f3bffbbfc18ccdcd0decc90303647e1ac3
Edward <Edward@DESKTOP-LGMB6VN>
2024-11-12 23:35:18 +0300

    Add project files.

---
+++ deneme/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deneme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region double değişken
+            //double number= 8.25;
+            //Console.WriteLine(number);
+            //double applePrice =14.85,
+            //    orangePrice=20.95,
+            //    strawberryPrice=45,
+            //    potatoPrice=9.74, 
+            //    tomatoPrice=6.88;
+            //Console.WriteLine("----- Elma birim fiyatı:" + applePrice+"TL");
+            //Console.WriteLine("----- Portakal birim fiyatı:" + orangePrice+"TL");
+            //Console.WriteLine("----- Çilek birim fiyatı:" + strawberryPrice+"TL");
+            //Console.WriteLine("----- Patates birim fiyatı:" + potatoPrice+"TL");
+            //Console.WriteLine("----- Domates birim fiyatı:" + tomatoPrice+"TL");
+            //double appleGram = 1.2,
+            //    orangeGram = 2.65,
+            //    strawberryGram = 0.75,
+            //    pototoGram = 4.859,
+            //    tomatoGram = 3.857;
+            //double appleTotalPrice = applePrice * appleGram,
+            //    strowberryTotalPrice = strawberryPrice * strawberryGram,
+            //    orangeTotalPrice = orangePrice * orangeGram,
+            //    pototoTotalPrice = potatoPrice * pototoGram,
+            //    tomatoTotalPrice = tomatoPrice * tomatoGram;
+
+            //double totalPrice = appleTotalPrice + orangeTotalPrice + strowberryTotalPrice + pototoTotalPrice + tomatoTotalPrice;
+            //Console.WriteLine(totalPrice);
+            //Console.ReadLine();
+            #endregion
+
+            int sayi;
+            Console.Write("sayı girişi:");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            Console.ReadKey();
+        }
+    }
+}
