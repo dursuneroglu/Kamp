a2f98c0ce48a39ab79693329b86a697f4ce486f9
Edward <Edward@DESKTOP-LGMB6VN>
2024-12-25 00:25:30 +0300

    Database

---
+++ 08_Methods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Metotlar
+            void costumerList()
+            {
+                Console.WriteLine("Ali");
+                Console.WriteLine("Mehmet");
+            }
+            costumerList();
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
